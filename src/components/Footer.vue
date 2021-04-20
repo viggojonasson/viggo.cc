8f6f0d540a56d0633f92a35109a13f95a08a0c4e
Viggo Jonasson <41396129+viggojonasson@users.noreply.github.com>
2021-04-20 15:01:43 +0200

    src: test

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="footer">
+        <div>
+            <p>discord: aethez#6403</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+    name: 'Footer'
+});
+</script>
+
+<style>
+.footer {
+    height: 4rem;
+    display: grid;
+    place-items: center;
+}
+</style>
